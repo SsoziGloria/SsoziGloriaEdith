87c1d16a11e6cc86488ac5bac2fc697b07ff5d08
SsoziGloria <gloshelisha@gmail.com>
2026-06-11 07:59:31 +0300

    exercises done duuring class

---
+++ exercise2.py
@@ -0,0 +1,18 @@
+day_number = int(input("Enter a number (1-7) to determine the day of the week: "))
+match day_number:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Invalid day number. Please enter a number between 1 and 7.")
